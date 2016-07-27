b105c281f67278e28a843eddbf08f6fc2bfb397f
Sagar Munjal <munjal.sagar@gmail.com>
2016-07-27 18:01:24 +0530

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+foo() ;  // function foo invocation 
+
+console.log(foo); //  undefined
+
+function foo () {console.log("sagar")}  // function definition 
+
+console.log(foo); // function () {console.log("sagar")}
+
